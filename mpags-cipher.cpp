d54f749567edfd692b061fb3c1c8382d0420bbcd
Ross Hunter <ross.hunter@warwick.ac.uk>
2018-10-12 12:14:27 +0100

    Tidied up previous code and added comments for new funk.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -62,6 +62,12 @@ bool processCommandLine(
 	std::string& inputFile,
 	std::string& outputFile ) 
 {
+  /* This function references to the string of command line args, 
+     and the other self-explanatory variables, and processes them
+     by reference. Throws errors if the arguments you gave it   
+     were incorrect. Gives a help & version statement if asked.
+  */
+
   // Add a typedef that assigns another name for the given type for clarity
   typedef std::vector<std::string>::size_type size_type;
   const size_type nCmdLineArgs {cmdLineArgs.size()};
@@ -150,95 +156,13 @@ int main(int argc, char* argv[])
   bool versionRequested {false};
   std::string inputFile {""};
   std::string outputFile {""};
-
+  
+  // Process the command line arguments and check if it went through OK. Kill if not. 
   bool status {processCommandLine(cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile)};
-
   if (status==false) {
 	return 0; 
   } 
-/*
-  // Add a typedef that assigns another name for the given type for clarity
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {cmdLineArgs.size()};
 
-  // Options that might be set by the command-line arguments
-  bool helpRequested {false};
-  bool versionRequested {false};
-  std::string inputFile {""};
-  std::string outputFile {""};
-
-  // Process command line arguments - ignore zeroth element, as we know this to
-  // be the program name and don't need to worry about it
-  for (size_type i {1}; i < nCmdLineArgs; ++i) {
-
-    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-      helpRequested = true;
-    }
-    else if (cmdLineArgs[i] == "--version") {
-      versionRequested = true;
-    }
-    else if (cmdLineArgs[i] == "-i") {
-      // Handle input file option
-      // Next element is filename unless -i is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -i requires a filename argument" << std::endl;
-	// exit main with non-zero return to indicate failure
-	return 1;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	inputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else if (cmdLineArgs[i] == "-o") {
-      // Handle output file option
-      // Next element is filename unless -o is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -o requires a filename argument" << std::endl;
-	// exit main with non-zero return to indicate failure
-	return 1;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	outputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else {
-      // Have an unknown flag to output error message and return non-zero
-      // exit status to indicate failure
-      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
-      return 1;
-    }
-  }
-
-  // Handle help, if requested
-  if (helpRequested) {
-    // Line splitting for readability
-    std::cout
-      << "Usage: mpags-cipher [-i <file>] [-o <file>]\n\n"
-      << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
-      << "Available options:\n\n"
-      << "  -h|--help        Print this help message and exit\n\n"
-      << "  --version        Print version information\n\n"
-      << "  -i FILE          Read text to be processed from FILE\n"
-      << "                   Stdin will be used if not supplied\n\n"
-      << "  -o FILE          Write processed text to FILE\n"
-      << "                   Stdout will be used if not supplied\n\n";
-    // Help requires no further action, so return from main
-    // with 0 used to indicate success
-    return 0;
-  }
-
-  // Handle version, if requested
-  // Like help, requires no further action,
-  // so return from main with zero to indicate success
-  if (versionRequested) {
-    std::cout << "0.1.0" << std::endl;
-    return 0;
-  }
-*/
   // Initialise variables for processing input text
   char inputChar {'x'};
   std::string inputText {""};
